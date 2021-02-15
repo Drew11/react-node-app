faddbe1118ccead2f0e5b2709153bdcbd2a2c00d
Drew11 <drew.naumenko@gmail.com>
2021-02-15 12:29:00 +0200

    done

--- src/components/csv-parser/csv-parser.js
+++ src/components/csv-parser/csv-parser.js
@@ -1,21 +1,14 @@
-import React, {useState, useRef} from 'react';
-import {
-    validateExperience,
-    validatePhone,
-    validateEmail,
-    validateName,
-    validateAge,
-    validateLicense} from './validators/validators';
+import React, { useState, useRef } from 'react';
+import { validateTable } from './validators/validators';
 import './csv-parser.scss';
-
-
+import { parseCSVtoArrayObjects } from './helpes/helpers'
 const CsvParser = ()=> {
 
     const [mapTable, setMapTable] = useState(null);
     const [error, setError] = useState(null);
     const csvInputRef = useRef();
 
-    const handleFileSelect = (event)=>{
+    const handleFileSelect = ( event ) => {
 
         const arr = event.target.files[0].name.split('.');
 
@@ -29,139 +22,58 @@ const CsvParser = ()=> {
         reader.onload = handleFileLoad;
         csvInputRef.current.value = "";
     };
+    const handleFileLoad = ( event ) => {
+        const convertRes = parseCSVtoArrayObjects(event.target.result);
 
-    const handleFileLoad =(event)=>{
-
-       setMapTable(parseCSVtoArrayObject(event.target.result));
+        if(typeof convertRes === 'string') {
+            setError(convertRes);
+            setMapTable(null);
+        }else {
+            setMapTable(validateTable(convertRes));
+            setError(null);
+        }
     };
 
-    function parseCSVtoArrayObject(csv) {
-        const lines = csv.trim().split("\n");
-
-        const array = lines.map((value, index)=>{
-            const arr = value.split(',').map(i=>i.trim());
-            if(index === 0) {
-                arr.unshift('Id')
-            }else
-                arr.unshift(index);
-            return arr;
-        } );
-
-        const jurists = [];
-        const requiredFields = ['Full Name', 'Phone', 'Email'];
-        const indices = requiredFields.map(field=>array[0].indexOf(field));
-
-        for (let i = 1; i < array.length; i++ ){
-            const jurist = {};
-
-
-            for (let k = 0; k < array[i].length; k ++){
-                if(array[i][k] === '' && indices.includes(k)){
-                    setError('Table not valid');
-                    return
-                }
-
-                if(array[0][k] === 'Phone'){
-                    if(array[i][k].length === 10){
-                        array[i][k] =`+1${array[i][k]}`
-                    }else
-                        array[i][k] = `+${array[i][k]}`
-                }
-
-                if(array[0][k] === 'Experience'){
-                    if(array[i][k] === ''){
-                        array[i][k] = "None"
-                    }
-                }
-
-                jurist[array[0][k]] = { value : array[i][k],valid: true };
-            }
-            jurist['Duplicate with'] = {valid: true};
-            jurists.push(jurist);
-        }
-
-        return jurists;
-    }
-
-    function validateTable() {
-        setMapTable(prevState=>{
-          return prevState.map(jurist=>{
-
-              const keys = Object.keys(jurist);
-              const copy = {...jurist};
-              copy['Experience'].valid = validateExperience(copy['Experience'].value, copy['Age'].value)
-
-              keys.forEach(key=>{
-
-                  if(key ==='License number'){
-                      copy[key].valid = validateLicense(copy[key].value);
-                  }
-
-                  if(key ==='Phone'){
-                      copy[key].valid = validatePhone(copy[key].value)
-                  }
-
-                  if(key ==='Age'){
-                      copy[key].valid = validateAge(copy[key].value)
-                  }
-
-                  if (key === 'Full Name'){
-                      copy[key].valid = validateName(copy[key].value)
-                  }
-
-                  if(key === 'Email'){
-                      copy[key].valid = validateEmail(copy[key].value)
-                  }
-
-                  if(key ==='Has children'){
-                      let boolean;
-                      if(copy[key].value === ''){
-                          copy[key].value = "FALSE"
-                      } else {
-                          boolean = typeof Boolean( copy[key].value) === "boolean";
-                      }
-                      copy[key].valid = boolean;
-                  }
-              });
-              return copy;
-          })
-        })
-
-    }
-
-    const crateHeaders = ()=> {
+    const crateHeaders = () => {
         return Object.keys(mapTable[0]).map((val, i)=>{
-            if(val === 'Duplicate with'){
-                return <th
-                    key={i}
-                    onClick={validateTable}
-                >
-                    {val}
-                </th>
-            }
-            return <th>{val}</th>
+            return <th key={i}>{val}</th>
         });
     };
 
-    const createRows = ()=>{
-        return mapTable.map(row=>{
-
-            return <tr>
+    const createRows = () => {
+        return mapTable.map((row, i)=>{
+            return <tr
+                key={i}
+            >
                 {
-                    Object.keys(row).map(key=>{
-
+                    Object.keys(row).map((key, idx)=>{
+                       const value = Array.isArray(row[key].value)? row[key].value.join(', '): row[key].value;
                        return <td
-                           className={row[key].valid?'':'not-valid'}
+                           className={row[key].valid ?'':'not-valid'}
                        >
-                           {row[key].value}
+                           { value }
                        </td>
                     })
                 }
             </tr>
         });
     };
+    const tableHeaders = mapTable && crateHeaders();
+    const tableRows = mapTable && createRows();
+    const errorView = error && <div className="error" >
+        <div>
+            <span>
+                { error }
+            </span>
+        </div>
 
-    console.log(mapTable)
+        <button
+            onClick={()=>setError(null)}
+        >
+            &#x2715;
+        </button>
+
+    </div>;
 
     return (
         <div className="csv-parser">
@@ -197,24 +109,20 @@ const CsvParser = ()=> {
                 }
             </div>
 
-
-            {error && <div>
-                {error}
-            </div>}
+            { errorView }
 
             <table>
                 <thead>
                     <tr>
-                        { mapTable && crateHeaders() }
+                        { tableHeaders }
                     </tr>
                 </thead>
 
                 <tbody>
-
-                    {  mapTable && createRows() }
-
+                    {   tableRows }
                 </tbody>
             </table>
+
         </div>
     );
 };
--- src/components/csv-parser/validators/validators.js
+++ src/components/csv-parser/validators/validators.js
@@ -1,14 +1,71 @@
-export function validateEmail(email) {
+import { getCurrentDateObject } from '../helpes/helpers';
+
+export  function validateTable (table) {
+    return table.map(jurist=>{
+
+        const keys = Object.keys(jurist);
+        const copy = {...jurist};
+        copy['Experience'].valid = validateExperience(copy['Experience'].value, copy['Age'].value)
+
+        keys.forEach(key=>{
+
+            if(key ==='License number' ){
+                copy[key].valid = validateLicense(copy[key].value);
+            }
+
+            if(key ==='Phone' && copy[key].valid !== false){
+                copy[key].valid = validatePhone(copy[key].value)
+            }
+
+            if(key ==='Age'){
+                copy[key].valid = validateAge(copy[key].value)
+            }
+
+            if (key === 'Full Name'){
+                copy[key].valid = validateName(copy[key].value)
+            }
+
+            if(key === 'Email'){
+                copy[key].valid = validateEmail(copy[key].value)
+            }
+
+            if(key ==='Has children'){
+                copy[key].valid = validateHasChildren(copy[key]);
+            }
+
+            if(key ==='Yearly Income'){
+                copy[key].valid = validateYearlyIncome(copy[key].value);
+            }
+
+            if(key ==='Expiration date'){
+                copy[key].valid = validateExpirationDate(copy[key].value);
+            }
+
+        });
+        return copy;
+    });
+
+}
+
+function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
-export function validateName(name) {
+function validateName(name) {
     const re = /^[a-zA-Z\s]*$/;
     return re.test(String(name).toLowerCase());
 }
 
-export function validateAge(age) {
+function validateYearlyIncome(string) {
+    if(string > 1000000){
+        return false
+    }
+    const re = /^\d{0,9}(\.\d{1,2})?$/;
+    return re.test(String(string).toLowerCase());
+}
+
+function validateAge(age) {
     if(age < 21) {
         return false
     }
@@ -16,15 +73,15 @@ export function validateAge(age) {
     return re.test(String(age).toLowerCase());
 }
 
-export function validateLicense(license) {
-    if (license.length < 6){
+function validateLicense(license) {
+    if (license.length !== 6){
         return false
     }
     const re = /^[a-zA-Z0-9]+$/;
     return re.test(String(license).toLowerCase());
 }
 
-export function validateExperience(experience, age) {
+function validateExperience(experience, age) {
     if(experience > (age - 21)){
         return false
     }
@@ -35,7 +92,7 @@ export function validateExperience(experience, age) {
     return re.test(String(experience).toLowerCase());
 }
 
-export function validatePhone(phone) {
+function validatePhone(phone) {
     const string = phone.slice(1, phone.length);
 
     if(string.length < 11) {
@@ -44,4 +101,70 @@ export function validatePhone(phone) {
     }
     const re = /^\d+$/;   //Positive Integer
     return re.test(String(string).toLowerCase());
+}
+
+function validateHasChildren(obj) {
+    let boolean = false;
+
+    if(obj.value  === ''){
+        obj.value = "FALSE";
+        boolean = true;
+    } else {
+        boolean = obj.value.toLowerCase() === 'false' || obj.value.toLowerCase() === 'true'
+    }
+    return boolean;
+}
+
+function validateExpirationDate(str) {
+
+    if(str.length !== 10 || str === ''){
+        return false
+    }
+    let boolean = false;
+
+    const re = /\d{4}-\d{2}-\d{2}/;
+    const re1 = /\d{2}-\d{2}-\d{4}/;
+    const bool = re.test(String(str));
+    const bool1 = re1.test(String(str));
+
+    if(bool || bool1) {
+        const arr = str.split('-');
+        const date = getCurrentDateObject();
+        boolean = true;
+                    //(YYYY-MM-DD або MM/DD/YYYY).
+        if(bool) {
+            arr.forEach((i, index)=>{
+
+                if(index === 0 && i < date.yyyy){
+                    boolean = false;
+                }
+
+                if(index === 1 && (i < date.mm || i > 12) ){
+                    boolean = false;
+                }
+
+                if(index === 2 && (i < date.dd || i > 31) && arr[1] < date.mm ){
+                    boolean = false;
+                }
+            });
+
+        }
+        if(bool1) {
+            arr.forEach((i, index)=>{
+                if(index === 0 && (i < date.mm || i > 12)){
+                    boolean = false;
+                }
+
+                if(index === 1 && (i < date.dd || i > 31) && arr[0] < date.mm){
+                    boolean = false;
+                }
+
+                if(index === 2 && i < date.yyyy){
+                    boolean = false;
+                }
+            });
+        }
+    }
+
+    return boolean;
 }
